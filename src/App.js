710b9367f1ec36c898120852bbc7c55b16ebbcff
Quinton Fults <quinton.fults.work@gmail.com>
2021-04-07 13:28:58 -0700

    code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import { useState } from 'react'
+
 const App = () => {
+  const [countState, setCountState] = useState({
+    name: 'John Doe',
+    count: 0
+  })
+
+  const handleIncrement = () => {
+    setCountState({ ...countState, count: countState.count + 1 })
+  }
+  const handleDecrement = () => {
+    setCountState({ ...countState, count: countState.count - 1 })
+  }
+
   return (
-    <h1>Hello World!</h1>
+    <>
+      <h1>Name: {countState.name}</h1>
+      <h1>Count: {countState.count}</h1>
+      <button onClick={handleIncrement}>+</button>
+      <button onClick={handleDecrement}>-</button>
+    </>
   )
 }
 
